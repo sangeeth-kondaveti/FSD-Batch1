dcc05738b15985b812a2047a703711b61dd58ca6
SangeethaShanmugm <MY_NAME@example.com>
2023-05-07 11:56:36 +0530

    added formik

--- Book-App/src/BasicForm.js
+++ Book-App/src/BasicForm.js
@@ -1,8 +1,21 @@
 import { useFormik } from "formik";
+import * as yup from "yup";
+
+const formValidationSchema = yup.object({
+  email: yup
+    .string()
+    .min(5, "Need a longer email🥳")
+    .required("Why not fill the email🥳"),
+  password: yup
+    .string()
+    .min(8, "Need a longer password🥳")
+    .required("Why not fill the password🥳"),
+});
 
 export function BasicForm() {
   const formik = useFormik({
     initialValues: { email: "", password: "" },
+    validationSchema: formValidationSchema,
     onSubmit: (values) => {
       console.log("onSubmit", values);
     },
@@ -17,7 +30,11 @@ export function BasicForm() {
         placeholder="Email"
         value={formik.values.email}
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
       />
+      <br />
+      {formik.touched.email && formik.errors.email ? formik.errors.email : ""}
+      <br />
       <input
         id="password"
         name="password"
@@ -25,7 +42,13 @@ export function BasicForm() {
         placeholder="Password"
         value={formik.values.password}
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
       />
+      <br />
+      {formik.touched.password && formik.errors.password
+        ? formik.errors.password
+        : ""}
+      <br />
       <button type="submit">Submit</button>
     </form>
   );
