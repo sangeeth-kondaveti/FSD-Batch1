4645f1f6dd07392232636273d1fe5ce3dd8f908c
SangeethaShanmugm <MY_NAME@example.com>
2023-05-07 13:38:07 +0530

    added formik to AddBook

--- Book-App/src/AddBook.js
+++ Book-App/src/AddBook.js
@@ -3,75 +3,177 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { API } from "./global";
 import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+//name - required
+//poster  - min 10, required
+//rating - 1-10, required,
+//summary - min 10 chars, required
+//trailer - min 4, required
+
+const BookValidationSchema = yup.object({
+  name: yup.string().required("Why not fill the name"),
+  poster: yup
+    .string()
+    .min(10, "Need a longer poster")
+    .required("Why not fill the poster"),
+  rating: yup
+    .number()
+    .min(1, "Need a higher rating")
+    .max(10, "Too much rating")
+    .required("Why not fill the rating"),
+  summary: yup
+    .string()
+    .min(10, "Need a longer summary")
+    .required("Why not fill the summary"),
+  trailer: yup
+    .string()
+    .min(4, "Need a longer trailer")
+    .required("Why not fill the trailer"),
+});
+
 export function AddBook() {
-  const [name, setName] = useState("");
-  const [poster, setPoster] = useState("");
-  const [rating, setRating] = useState("");
-  const [summary, setSummary] = useState("");
-  const [trailer, setTrailer] = useState("");
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      poster: "",
+      rating: "",
+      summary: "",
+      trailer: "",
+    },
+    validationSchema: BookValidationSchema,
+    onSubmit: (newBook) => {
+      console.log("onSubmit", newBook);
+      createBook(newBook);
+    },
+  });
+
   const navigate = useNavigate();
+
+  const createBook = (newBook) => {
+    fetch(`${API}/books`, {
+      method: "POST",
+      body: JSON.stringify(newBook),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((data) => data.json())
+      .then(() => navigate("/books"));
+  };
+
   return (
-    <div className="add-book-form">
-      <TextField
-        label="Name"
-        variant="outlined"
-        onChange={(event) => setName(event.target.value)}
-      />
+    <form onSubmit={formik.handleSubmit}>
+      <div className="add-book-form">
+        <TextField
+          label="Name"
+          variant="outlined"
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error
+          helperText={"nice"}
+        />
+        {formik.touched.name && formik.errors.name ? formik.errors.name : ""}
 
-      <TextField
-        label="Poster"
-        variant="outlined"
-        onChange={(event) => setPoster(event.target.value)}
-      />
-      <TextField
-        label="Rating"
-        variant="outlined"
-        onChange={(event) => setRating(event.target.value)}
-      />
-      <TextField
-        label="Summary"
-        variant="outlined"
-        onChange={(event) => setSummary(event.target.value)}
-      />
+        <TextField
+          label="Poster"
+          variant="outlined"
+          id="poster"
+          name="poster"
+          type="text"
+          placeholder="Poster"
+          value={formik.values.poster}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.poster && formik.errors.poster
+          ? formik.errors.poster
+          : ""}
 
-      <TextField
-        label="Trailer"
-        variant="outlined"
-        onChange={(event) => setTrailer(event.target.value)}
-      />
-      {/* copy the bookList and newbook */}
+        <TextField
+          label="Rating"
+          variant="outlined"
+          id="rating"
+          name="rating"
+          type="number"
+          placeholder="Rating"
+          value={formik.values.rating}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.rating && formik.errors.rating
+          ? formik.errors.rating
+          : ""}
 
-      <Button
-        variant="contained"
-        onClick={() => {
-          const newBook = {
-            name: name,
-            poster: poster,
-            rating: rating,
-            summary: summary,
-            trailer: trailer,
-          };
-          //1. method -POST
-          //2. body - data -JSON.stringify
-          //3. headers - JSON
+        <TextField
+          label="Summary"
+          variant="outlined"
+          id="summary"
+          name="summary"
+          type="text"
+          placeholder="Summary"
+          value={formik.values.summary}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.summary && formik.errors.summary
+          ? formik.errors.summary
+          : ""}
 
-          fetch(`${API}/books`, {
-            method: "POST",
-            body: JSON.stringify(newBook),
-            headers: { "Content-Type": "application/json" },
-          })
-            .then((data) => data.json())
-            .then(() => navigate("/books"));
-          //currently post and navigate is immediate
-          // When post is completed -> navigate=> /books
+        <TextField
+          label="Trailer"
+          variant="outlined"
+          id="trailer"
+          name="trailer"
+          type="text"
+          placeholder="Trailer"
+          value={formik.values.trailer}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.trailer && formik.errors.trailer
+          ? formik.errors.trailer
+          : ""}
 
-          // setBookList([...bookList, newBook]);
-        }}
-      >
-        Add Book
-      </Button>
+        {/* copy the bookList and newbook */}
 
-      {/* <button
+        <Button
+          variant="contained"
+          onClick={createBook}
+          type="submit"
+          // onClick={() => {
+          //   const newBook = {
+          //     name: name,
+          //     poster: poster,
+          //     rating: rating,
+          //     summary: summary,
+          //     trailer: trailer,
+          //   };
+          //   //1. method -POST
+          //   //2. body - data -JSON.stringify
+          //   //3. headers - JSON
+
+          //   fetch(`${API}/books`, {
+          //     method: "POST",
+          //     body: JSON.stringify(newBook),
+          //     headers: { "Content-Type": "application/json" },
+          //   })
+          //     .then((data) => data.json())
+          //     .then(() => navigate("/books"));
+          //   //currently post and navigate is immediate
+          //   // When post is completed -> navigate=> /books
+
+          //   // setBookList([...bookList, newBook]);
+          // }}
+        >
+          Add Book
+        </Button>
+
+        {/* <button
                     onClick={() => {
                       const newBook = {
                         name: name,
@@ -84,6 +186,7 @@ export function AddBook() {
                   >
                     Add Book
                   </button> */}
-    </div>
+      </div>
+    </form>
   );
 }
